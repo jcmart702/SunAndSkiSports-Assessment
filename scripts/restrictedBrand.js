67a80d1995d362ecc4cb10cc84ef1763417ffccb
jcmarti702 <jcmart702@gmail.com>
2021-05-10 17:24:00 -0500

    IE 11 Restricted Brands Comptablity
    -Made restricted brands function IE 11 comptable

--- scripts/restrictedBrand.js
+++ scripts/restrictedBrand.js
@@ -1,10 +1,19 @@
 let restrictedList = ["Burton", "Patagonia", "Ride", "Thule", "Yeti"];
+let productNameElements = document.getElementsByClassName("product-name");
+
+for(let i = 0; i < productNameElements.length; i++){
+  productNameElements[i].addEventListener('click', function(e){
+    restrictedBrands(e.target);
+  })
+}
 
 function restrictedBrands(el){
   let productText = el.innerText;
+  let productTextSplit = productText.split(' ');
+  let brandText = productTextSplit[0];
 
-  for(let i=0; i < restrictedList.length; i++){
-    if(productText.startsWith(restrictedList[i])){
+  for(let i = 0; i < restrictedList.length; i++){
+    if(brandText === (restrictedList[i])){
       displayRestrictionMessage(el, restrictedList[i]);
       break;
     }
@@ -12,7 +21,7 @@ function restrictedBrands(el){
 };
 
 function displayRestrictionMessage(msg, brand){
-  let message = msg.previousElementSibling.childNodes[3];
+  let message = msg.parentNode.previousElementSibling.childNodes[3];
 
   message.innerText = "Sorry, " + brand + " is restricted.";
   message.style.visibility = "visible";
